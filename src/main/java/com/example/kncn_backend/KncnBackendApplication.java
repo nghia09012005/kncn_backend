9460db592484a2e5b0f0bdf6c8f0c0939cc67c11
Tran Nguyen Phu Nghia <nghiatran912005@gamil.com>
2025-03-07 09:32:18 +0700

    demo backend

--- src/main/java/com/example/kncn_backend/KncnBackendApplication.java
+++ src/main/java/com/example/kncn_backend/KncnBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KncnBackendApplication {
 
+	//push to remote
 	public static void main(String[] args) {
 		SpringApplication.run(KncnBackendApplication.class, args);
 	}
